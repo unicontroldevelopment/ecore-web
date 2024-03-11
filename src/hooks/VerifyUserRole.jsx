18e2bd1c3ff7a8eb365b1caa4df77897e669e428
Eduardo Colissi <colissi.eduardo@gmail.com>
2024-03-11 19:17:25 -0300

    ECW2 - 10 Access control

---
+++ src/hooks/VerifyUserRole.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
+import { UserTypeContext } from "../contexts/UserTypeContext";
+
+export function VerifyUserRole(userRoles) {
+  const navigate = useNavigate();
+  const userTypeContext = React.useContext(UserTypeContext);
+
+  React.useEffect(() => {
+    const verifyUser = () => {
+      if (!userRoles.includes(userTypeContext.userType)) {
+        navigate("/acessoNegado");
+      }
+    };
+    verifyUser();
+  }, []);
+}
+
+export default VerifyUserRole;
